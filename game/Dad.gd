b1da49a73a71e7ab95cab79d7397d74b251d8857
Daniel Grunkin <danielgrunkin@gmail.com>
2020-04-16 15:02:33 +0200

    yes!

--- game/Dad.gd
+++ game/Dad.gd
@@ -1,43 +1,9 @@
-extends KinematicBody2D
-
-var movespeed = 100
-
-onready var actions = $Actions
-
-var start_pos = Vector2()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	actions.connect("action_activated", self, "act")
-	start_pos = position
+extends "res://Character.gd"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var m = actions.move_direction()
-	move_and_collide(m * movespeed*delta)
-
-func act(evt):
-	if not evt.type == "pressed":
-		return
-	if evt.action == "action_1":
-		action_1()
-	if evt.action == "action_2":
-		action_2()
 
 func action_1():
 	print("dad humor")
 
 func action_2():
 	print("grill expert")
-
-func reset():
-	position = start_pos
-	actions.reset()
-
-
-func pause():
-	actions.pause()
-
-func choose():
-	actions.is_player_in_control = true
-
--- game/Granny.gd
+++ game/Granny.gd
@@ -1,44 +1,7 @@
-extends KinematicBody2D
-
-var movespeed = 100
-
-var start_pos = Vector2()
-
-onready var actions = $Actions
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	actions.connect("action_activated", self, "act")
-	start_pos = position
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var m = actions.move_direction()
-	move_and_collide(m * movespeed * delta)
-
-func act(evt):
-	if not evt.type == "pressed":
-		return
-	if evt.action == "action_1":
-		action_1()
-	if evt.action == "action_2":
-		action_2()
+extends "res://Character.gd"
 
 func action_1():
 	print("distracted guards with cookies")
 
 func action_2():
 	print("hit guard with handbag")
-
-
-func choose():
-	actions.is_player_in_control = true
-	
-
-func reset():
-	position = start_pos
-	actions.reset()
-
-func pause():
-	actions.pause()
