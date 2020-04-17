c307356a121ca1e134378179544f36dcdf073f14
JakobSchauser <jakob.schauser@gmail.com>
2020-04-18 00:07:25 +0200

    cant see shapes

--- game/SecurityCam.gd
+++ game/SecurityCam.gd
@@ -1,12 +1,14 @@
 extends Area
 
 signal caught
+
+var cancatch = true
 func _ready():
 	$Spot.connect("body_entered", self, "b")
 
 
 func b(body):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and cancatch:
 		emit_signal("caught",body,name)
 		catch()
 		
@@ -16,9 +18,18 @@ func catch():
 
 
 func hit():
+	cancatch = false
+	$Sprite/Light.light_energy = 0
 	$AnimationPlayer.play("Rotator")
 	
 func reset():
 	hit()
+	$Sprite/Light.light_energy = 1
+	cancatch = true
 	$AnimationPlayer.seek(0,true)
 	$AnimationPlayer.stop(true)
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	$Sprite/Light.light_energy = 1
+	cancatch = true
