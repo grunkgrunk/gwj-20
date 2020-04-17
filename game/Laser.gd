49fa1ba02e6be14a0deb3abbb9ac7874009c8b67
JakobSchauser <jakob.schauser@gmail.com>
2020-04-17 19:15:54 +0200

    made laser work

--- game/Laser.gd
+++ game/Laser.gd
@@ -4,15 +4,18 @@ signal caught
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var cancatch = true
 
 func start():
+	cancatch = false
 	$Timer.start()
+	hide()
 
 func _on_Area_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and cancatch:
 		emit_signal("caught",body,name)
 
 
 func _on_Timer_timeout():
-	pass # Replace with function body.
+	show()
+	cancatch = true
