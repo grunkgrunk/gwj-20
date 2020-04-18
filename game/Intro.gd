3e5cab9df410b87f88ab18bc421765f0044dd892
Daniel Grunkin <danielgrunkin@gmail.com>
2020-04-18 02:11:16 +0200

    intro now working

--- game/Intro.gd
+++ game/Intro.gd
@@ -1,21 +1,33 @@
 extends Control
 
 signal next_txt
+signal intro_text_done
 
 onready var l = $Label
 
 var texts = ["This is an intro", "You are driving", "You are about to play a fun game", "good luck"]
 
 func _ready():
-	intro()
+	pass
 
-func intro():
+func run_intro():
+	show()
 	for t in texts:
 		l.text = t
 		yield(self, "next_txt")
+	
+	emit_signal("intro_text_done")
+
+func run_tutor():
+	l.text = "Press escape to enter time machine!"
+	#yield(self, "next_txt")
+	#emit_signal("intro_text_done")
+	#hide()
+	
 
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		emit_signal("next_txt")
-		print("kjsdhf")
+
+
